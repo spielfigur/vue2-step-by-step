668f1a14aeb0368a11a339c15d8a0a98a19de3fd
Spielfigur <hajo.voet@t-online.de>
2019-01-16 14:31:43 +0100

    episode 17 added
    hot module replacement
    fixed wepack-dev-server alert

---
+++ episode 17/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      <counter></counter>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue';
+
+export default {
+  name: 'app',
+  components: {
+      Counter
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ episode 17/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>
+            The count is: {{ count }}
+        </h2>
+
+        <p>
+            <button @click="count += 7">Increment by 7</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
