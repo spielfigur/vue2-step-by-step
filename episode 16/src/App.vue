176616299d67f443eb353c009ec2498bc3bec76b
Spielfigur <hajo.voet@t-online.de>
2019-01-16 14:01:10 +0100

    episode 16 added
    installing vue-cli and webpack

---
+++ episode 16/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+      <message>
+          Hello there
+      </message>
+      <message>
+          Hello there again
+      </message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+
+export default {
+  name: 'app',
+  components: {
+      Message
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
